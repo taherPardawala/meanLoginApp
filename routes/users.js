2cc3bac511f44a50e6748f82a9be2ce2d3a94e1f
taherPardawala <skypunch2@gmail.com>
2017-06-30 22:57:11 +0530

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -56,24 +56,23 @@ router.post('/register', function(req, res){
 	}
 });
 
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-		User.getUserByUsername(username, function(err, user){
-   	if(err) throw err;
-   	if(!user){
-   		return done(null, false, {message: 'Unknown User'});
-   	}
+passport.use(new LocalStrategy(function(username, password, done) {
+	User.getUserByUsername(username, function(err, user){
+		if(err) throw err;
+		if(!user){
+			return done(null, false, {message: 'Unknown User'});
+		}
 
-   	User.comparePassword(password, user.password, function(err, isMatch){
-   		if(err) throw err;
-   		if(isMatch){
-   			return done(null, user);
-   		} else {
-   			return done(null, false, {message: 'Invalid password'});
-   		}
-   	});
+		User.comparePassword(password, user.password, function(err, isMatch){
+			if(err) throw err;
+			if(isMatch){
+				return done(null, user);
+			} else {
+				return done(null, false, {message: 'Invalid password'});
+			}
 		});
-	}));
+	});
+}));
 
 passport.serializeUser(function(user, done) {
 	done(null, user.id);
